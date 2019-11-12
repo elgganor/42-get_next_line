b6d4e071765f40194bbf9117a7dc01d4b7109cfb
mrouabeh <mrouabeh@student.42.fr>
2019-11-12 11:00:03 +0100

    Mise à la norme

--- get_next_line.c
+++ get_next_line.c
@@ -1,77 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrouabeh <mrouabeh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/12 10:50:34 by mrouabeh          #+#    #+#             */
+/*   Updated: 2019/11/12 10:59:22 by mrouabeh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-int check_error(int fd, char **line)
+int		check_error(int fd, char **line)
 {
-    if (fd < 0 || !line || BUFFER_SIZE < 1)
-        return (0);
-    return (1);
+	if (fd < 0 || !line || BUFFER_SIZE < 1)
+		return (0);
+	return (1);
 }
 
-int check_line_break(char *str)
+int		check_line_break(char *str)
 {
-    int index;
+	int	index;
 
-    index = 0;
-    if (!str)
-        return (-1);
-    while (str[index] != '\0')
-    {
-        if (str[index] == '\n')
-            return (index);
-        index++;
-    }
-    return (-1);
+	index = 0;
+	if (!str)
+		return (-1);
+	while (str[index] != '\0')
+	{
+		if (str[index] == '\n')
+			return (index);
+		index++;
+	}
+	return (-1);
 }
 
-void get_line(char **line, char **str, int index)
+void	get_line(char **line, char **str, int index)
 {
-    int i;
-    char *tmp;
+	int		i;
+	char	*tmp;
 
-    i = 0;
-    if (!(*line = (char *)ft_calloc(index + 1, sizeof(char))))
-        return ;
-    while (i < index)
-    {
-        (*line)[i] = (*str)[i];
-        i++;
-    }
-    (*line)[i] = '\0';
-    tmp = ft_strdup(*str);
-    free(*str);
-    *str = ft_strdup(tmp + index + 1);
-    free(tmp);
-    // tmp = NULL;
+	i = 0;
+	if (!(*line = (char *)ft_calloc(index + 1, sizeof(char))))
+		return ;
+	while (i < index)
+	{
+		(*line)[i] = (*str)[i];
+		i++;
+	}
+	(*line)[i] = '\0';
+	tmp = ft_strdup(*str);
+	free(*str);
+	*str = ft_strdup(tmp + index + 1);
+	free(tmp);
 }
 
-int get_next_line(int fd, char **line)
+int		get_next_line(int fd, char **line)
 {
-    static char *str = NULL;
-    int         r;
-    char        *buf;
+	static char	*str = NULL;
+	int			r;
+	char		*buf;
 
-    if (check_error(fd, line) == 0
-        || !(buf = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char))))
-        return (-1);
-    while (check_line_break(str) == -1 && (r = read(fd, buf, BUFFER_SIZE)) != 0)
-    {
-        if (r == -1)
-            return (-1);
-        buf[r] = '\0';
-        ft_strjoin_free(&str, buf);
-    }
-    free(buf);
-    if (check_line_break(str) != -1)
-    {
-        get_line(line, &str, check_line_break(str));
-        return (1);
-    }
-    if (r == 0 && (str != NULL))
-    {
-        *line = ft_strdup(str);
-        free(str);
-        str = NULL;
-        return (1);
-    }
-    return (0);
+	if (check_error(fd, line) == 0
+		|| !(buf = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char))))
+		return (-1);
+	while (check_line_break(str) == -1 && (r = read(fd, buf, BUFFER_SIZE)) != 0)
+	{
+		if (r == -1)
+			return (-1);
+		buf[r] = '\0';
+		ft_strjoin_free(&str, buf);
+	}
+	free(buf);
+	if (check_line_break(str) != -1)
+	{
+		get_line(line, &str, check_line_break(str));
+		return (1);
+	}
+	if (r == 0 && (str != NULL))
+	{
+		*line = ft_strdup(str);
+		free(str);
+		str = NULL;
+		return (1);
+	}
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrouabeh <mrouabeh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/12 10:53:30 by mrouabeh          #+#    #+#             */
+/*   Updated: 2019/11/12 10:53:54 by mrouabeh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <stdlib.h>
@@ -5,13 +17,13 @@
 # include <string.h>
 # include <limits.h>
 
-int get_next_line(int fd, char **line);
-size_t ft_strlen(const char *s);
-char *ft_strdup(const char *s);
-char *ft_strjoin(char *buf, char *tmp);
-void ft_strjoin_free(char **str, char *buf);
-void get_line(char **line, char **str, int index);
-int check_line_break(char *str);
-int check_error(int fd, char **line);
-void *ft_calloc(size_t count, size_t size);
+int		get_next_line(int fd, char **line);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char *buf, char *tmp);
+void	ft_strjoin_free(char **str, char *buf);
+void	get_line(char **line, char **str, int index);
+int		check_line_break(char *str);
+int		check_error(int fd, char **line);
+void	*ft_calloc(size_t count, size_t size);
 #endif
