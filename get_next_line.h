d4cfefd23d29e7bce875aaa38830eca5dc0ef703
Mohamed-Younès <mohamed.rouabehi.pro@gmail.com>
2019-11-12 09:28:42 +0100

    Ajout du header

---
+++ get_next_line.h
@@ -0,0 +1,17 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <limits.h>
+
+int get_next_line(int fd, char **line);
+size_t ft_strlen(const char *s);
+char *ft_strdup(const char *s);
+char *ft_strjoin(char *buf, char *tmp);
+void ft_strjoin_free(char **str, char *buf);
+void get_line(char **line, char **str, int index);
+int check_line_break(char *str);
+int check_error(int fd, char **line);
+void *ft_calloc(size_t count, size_t size);
+#endif
